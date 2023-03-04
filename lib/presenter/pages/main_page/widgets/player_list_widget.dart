b072acae018e0f433843937aabb3048bc9dee6b9
Matheus Ferreira <matheus.44300@gmail.com>
2023-03-04 00:49:59 -0300

    Update player_list_widget.dart

--- lib/presenter/pages/main_page/widgets/player_list_widget.dart
+++ lib/presenter/pages/main_page/widgets/player_list_widget.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
 
 class PlayerListWidget extends StatelessWidget {
+  final String title;
   final List<Widget> childrens;
   const PlayerListWidget({
     Key? key,
+    required this.title,
     required this.childrens,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            'Continue assistindo:',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              color: Colors.white,
-              fontSize: 12,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                fontSize: 12,
+              ),
             ),
           ),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.symmetric(horizontal: 4),
-          physics: const BouncingScrollPhysics(),
-          child: Row(children: childrens),
-        ),
-      ],
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            physics: const BouncingScrollPhysics(),
+            child: Row(children: childrens),
+          ),
+        ],
+      ),
     );
   }
 }
