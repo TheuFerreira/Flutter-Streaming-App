da43d66b0cd74a14123d3ba18565eabc5e9c3773
Matheus Ferreira <matheus.44300@gmail.com>
2023-03-04 00:50:19 -0300

    Update main_page.dart

--- lib/presenter/pages/main_page/main_page.dart
+++ lib/presenter/pages/main_page/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:streaming_app/presenter/pages/main_page/widgets/notification_button_widget.dart';
 import 'package:streaming_app/presenter/pages/main_page/widgets/player_list_widget.dart';
+import 'package:streaming_app/presenter/pages/main_page/widgets/playlist_item_widget.dart';
 import 'package:streaming_app/presenter/pages/main_page/widgets/user_info_widget.dart';
 import 'package:streaming_app/presenter/pages/main_page/widgets/video_item_widget.dart';
 
@@ -15,52 +16,90 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: SizedBox(
-                width: double.infinity,
-                height: 90,
-                child: Stack(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Column(
                   children: [
-                    Positioned(
-                      top: 1,
-                      right: 1,
-                      child: NotificationButtonWidget(
-                        normalColor: const Color(0xFF484848),
-                        notificationColor: const Color(0xFF967BE3),
-                        onTap: () {},
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        NotificationButtonWidget(
+                          normalColor: const Color(0xFF484848),
+                          notificationColor: const Color(0xFF967BE3),
+                          onTap: () {
+                            // TODO: Open Notifications
+                          },
+                        ),
+                      ],
                     ),
-                    Positioned(
-                      bottom: 1,
-                      left: 1,
-                      child: UserInfoWidget(
-                        photo: Image.asset('assets/temp/perfil.jpg'),
-                        username: 'Alessandra',
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        UserInfoWidget(
+                          photo: Image.asset('assets/temp/perfil.jpg'),
+                          username: 'Alessandra',
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-            PlayerListWidget(
-              childrens: [
-                VideoItemWidget(
-                  image: Image.asset('assets/temp/01.jpg').image,
-                  description: 'Temporada 2: Episódio 10',
-                ),
-                VideoItemWidget(
-                  image: Image.asset('assets/temp/01.jpg').image,
-                  description: 'Temporada 2: Episódio 10',
-                )
-              ],
-            ),
-          ],
+              const SizedBox(height: 24),
+              PlayerListWidget(
+                title: 'Continue assistindo:',
+                childrens: [
+                  VideoItemWidget(
+                    image: Image.asset('assets/temp/01.jpg').image,
+                    description: 'Temporada 2: Episódio 10',
+                  ),
+                  VideoItemWidget(
+                    image: Image.asset('assets/temp/01.jpg').image,
+                    description: 'Temporada 2: Episódio 10',
+                  )
+                ],
+              ),
+              PlayerListWidget(
+                title: 'Em alta:',
+                childrens: [
+                  PlaylistItemWidget(
+                    image: Image.asset('assets/temp/02.jpg').image,
+                    description: 'La Casa de Papel',
+                  ),
+                  PlaylistItemWidget(
+                    image: Image.asset('assets/temp/02.jpg').image,
+                    description: 'La Casa de Papel',
+                  ),
+                  PlaylistItemWidget(
+                    image: Image.asset('assets/temp/02.jpg').image,
+                    description: 'La Casa de Papel',
+                  ),
+                ],
+              ),
+              PlayerListWidget(
+                title: 'Lançamentos:',
+                childrens: [
+                  PlaylistItemWidget(
+                    image: Image.asset('assets/temp/02.jpg').image,
+                    description: 'La Casa de Papel',
+                  ),
+                  PlaylistItemWidget(
+                    image: Image.asset('assets/temp/02.jpg').image,
+                    description: 'La Casa de Papel',
+                  ),
+                  PlaylistItemWidget(
+                    image: Image.asset('assets/temp/02.jpg').image,
+                    description: 'La Casa de Papel',
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
