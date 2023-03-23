c85778c8ef7ba534ff5d52f4a0c5a7d8af1c69c0
Matheus Ferreira <matheus.44300@gmail.com>
2023-03-22 23:08:51 -0300

    Added login controller

---
+++ lib/presenter/pages/login/login_controller.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+import 'package:streaming_app/presenter/pages/login/dialogs/info_dialog.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = LoginControllerBase with _$LoginController;
+
+abstract class LoginControllerBase with Store {
+  @observable
+  bool rememberMe = false;
+
+  @action
+  void resetPassword(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (builder) {
+        return const InfoDialog(
+          icon: Icons.email,
+          description:
+              'Um link para redefinição\nde senha foi enviado para\nseu e-mail!',
+          buttonText: 'Ok',
+        );
+      },
+    );
+  }
+
+  @action
+  void setRememberMe(bool value) {
+    rememberMe = value;
+  }
+
+  @action
+  void signIn(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (builder) {
+        return const InfoDialog(
+          icon: Icons.email,
+          description: 'Você ainda não\nconfirmou seu e-mail!',
+          buttonText: 'Ok',
+        );
+      },
+    );
+
+    showDialog(
+      context: context,
+      builder: (builder) {
+        return const InfoDialog(
+          icon: Icons.error,
+          iconColor: Colors.red,
+          description: 'Ocorreu algum erro interno,\ntente novamente!',
+          buttonText: 'Tentar novamente',
+        );
+      },
+    );
+
+    showDialog(
+      context: context,
+      builder: (builder) {
+        return const InfoDialog(
+          icon: Icons.warning,
+          iconColor: Colors.yellow,
+          description: 'Usuário ou senha\nincorretos!',
+          buttonText: 'Ok',
+        );
+      },
+    );
+  }
+
+  @action
+  void register() {}
+}
---
+++ lib/presenter/pages/login/login_controller.g.dart
@@ -0,0 +1,81 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$LoginController on LoginControllerBase, Store {
+  late final _$rememberMeAtom =
+      Atom(name: 'LoginControllerBase.rememberMe', context: context);
+
+  @override
+  bool get rememberMe {
+    _$rememberMeAtom.reportRead();
+    return super.rememberMe;
+  }
+
+  @override
+  set rememberMe(bool value) {
+    _$rememberMeAtom.reportWrite(value, super.rememberMe, () {
+      super.rememberMe = value;
+    });
+  }
+
+  late final _$LoginControllerBaseActionController =
+      ActionController(name: 'LoginControllerBase', context: context);
+
+  @override
+  void resetPassword(BuildContext context) {
+    final _$actionInfo = _$LoginControllerBaseActionController.startAction(
+        name: 'LoginControllerBase.resetPassword');
+    try {
+      return super.resetPassword(context);
+    } finally {
+      _$LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setRememberMe(bool value) {
+    final _$actionInfo = _$LoginControllerBaseActionController.startAction(
+        name: 'LoginControllerBase.setRememberMe');
+    try {
+      return super.setRememberMe(value);
+    } finally {
+      _$LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void signIn(BuildContext context) {
+    final _$actionInfo = _$LoginControllerBaseActionController.startAction(
+        name: 'LoginControllerBase.signIn');
+    try {
+      return super.signIn(context);
+    } finally {
+      _$LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void register() {
+    final _$actionInfo = _$LoginControllerBaseActionController.startAction(
+        name: 'LoginControllerBase.register');
+    try {
+      return super.register();
+    } finally {
+      _$LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+rememberMe: ${rememberMe}
+    ''';
+  }
+}
