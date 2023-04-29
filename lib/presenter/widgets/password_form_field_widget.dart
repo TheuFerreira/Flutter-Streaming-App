6a2bdcb6ef89ba30c9eac01bb56489fe1e271219
Matheus Ferreira <matheus.44300@gmail.com>
2023-04-28 22:38:33 -0300

    Update password_form_field_widget.dart

--- lib/presenter/widgets/password_form_field_widget.dart
+++ lib/presenter/widgets/password_form_field_widget.dart
@@ -11,6 +11,7 @@ class PasswordFormFieldWidget extends FormFieldWidget {
     required super.validator,
     required super.obscureText,
     required this.onChangeObscureText,
+    super.onChanged,
   });
 
   @override
